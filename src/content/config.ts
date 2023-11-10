3d6d1a95a5be3ac7fdcac783a7cb543a1d5b3a36
Salwa-mansour <alqadasisal@gmail.com>
2023-11-10 22:44:20 +0300

    trying to add blog ,getting errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,12 +19,16 @@ const postsCollection = defineCollection({
   
 const projectsCollection =  defineCollection({
     type: 'content', // v2.5.0 and later
-    schema: z.object({
+    schema: ({ image }) => z.object({
       title: z.string(),
+     
+      cover: image().refine((img) => img.width >= 400, {
+        message: "Cover image must be at least 400 pixels wide!",
+      }),
       excerpt: z.string(),
-      cover: z.string().optional(),
-    })
-  })
+    
+    }),
+  });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
