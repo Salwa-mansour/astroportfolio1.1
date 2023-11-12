6e2313ea960188fd5b38a2c9f0a6665e61a9c1b9
Salwa-mansour <alqadasisal@gmail.com>
2023-11-12 23:09:36 +0300

    trying to add multi langouge blog ,still having routing errors

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'English',
+    fr: 'Arabic',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'title.blog': 'blog',
+      'title.more': 'more',
+      'title.twitter': 'Twitter',
+    },
+    ar: {
+      'title.blog': 'المدونة',
+      'title.more': 'المزيد',
+    },
+  } as const;
+  export const showDefaultLang = false;
